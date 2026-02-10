d74989f7f814f28fd5c990f9b8d4d6da3cab38d1
ManiMindScripts <ar7279796@gmail.com>
2026-02-10 15:17:16 +0500

    Task 5 Completed

--- src/Task5/features/products/components/TableSearch.tsx
+++ src/Task5/features/products/components/TableSearch.tsx
@@ -1,40 +1,34 @@
-import { useState, useEffect } from "react";
+import { useDeferredValue, useEffect } from "react";
 import { Search } from "lucide-react";
 
 interface SearchBarPropos {
     value: string
     onChange: (value: string) => void
     placeholder?: string
-    debounceMs?: number
+
 }
 export function TableSearch({
     value,
     onChange,
     placeholder = "Search...",
-    debounceMs = 300,
-}: SearchBarPropos){
-     const [inputValue, setInputValue] = useState(value)
+}: SearchBarPropos) {
+    const deferredValue = useDeferredValue(value)
 
-     useEffect(()=> {
-    const  handler = setTimeout(() => {
-      onChange(inputValue)
-     },debounceMs)
-     return () => {
-        clearTimeout(handler)
-     }
-},[inputValue,onChange,debounceMs])
-        return(
-            <>
+    useEffect(() => {
+        onChange(deferredValue)
+    }, [deferredValue, onChange])
+    return (
+        <>
             <div className="items-center flex border gap-2 rounded-lg px-3 py-2 w-full max-w-sm bg-white shadow-sm mb-2">
-               <Search className="w-5 h-5 text-gray-500"/>
-               <input
-               type="text"
-               placeholder={placeholder}
-               value={inputValue}
-               onChange={(e) => setInputValue(e.target.value)}
-               className="outline-none w-full"
-               />
+                <Search className="w-5 h-5 text-gray-500" />
+                <input
+                    type="text"
+                    placeholder={placeholder}
+                    value={value}
+                    onChange={(e) => onChange(e.target.value)}
+                    className="outline-none w-full"
+                />
             </div>
-            </>
-        )
+        </>
+    )
 }
